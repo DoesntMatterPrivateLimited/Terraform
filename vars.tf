92f24facb31ab68d1b6c2f953b1b750a42dc1eb5
vaibankar <113019542+vaibankar@users.noreply.github.com>
2024-03-03 13:58:21 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -11,6 +11,10 @@ variable "region" {
 variable "environment" {
   description = "Deployment Environment"
 }
+variable "project" {
+    description = "Project Environment"
+  
+}
 
 variable "cidr_block" {
   description = "CIDR block of the vpc"
@@ -22,20 +26,15 @@ variable "public_subnets_cidr" {
   description = "CIDR block for Public Subnet"
   default     = ["20.0.1.0/24", "20.0.2.0/24"]
 }
-
 variable "private_subnets_cidr" {
   type        = list(any)
   description = "CIDR block for Private Subnet"
   default     = ["20.0.10.0/24"]
 }
-
 variable "instance_type" {
   description = "This is a instance type"
   default = "t2.micro"
-  
-
 }
-
 variable "ami" {
   description = "This is ami id"
   default     = "ami-05fb0b8c1424f266b"
@@ -44,4 +43,26 @@ variable "ami" {
 variable "key_name" {
   description = "The name of the AWS key pair"
   default     = "tf-key"
+}
+variable "ecr_name" {
+    description = "The name of the repository"
+    type = string   
+  
+}
+variable "tags" {
+    description = "The key-value maps for tagging"
+    type = map(string)
+    default = {}
+}
+variable "image_mutability" {
+    description = "Provide image mutability"
+    type = string
+    default = "MUTABLE"
+  
+}
+variable "encryption_type" {
+    description = "Provide type of encryption here"
+    type = string
+    default = "KMS"
+  
 }
